41316d98ec6d793b3a86ff07d828cf983870fc79
Thricons <BetaTestNMSL@163.com>
2023-03-11 11:50:23 +0800

    s8ovy90

--- src/main/java/cn/exception/module/impl/movement/Scaffold.java
+++ src/main/java/cn/exception/module/impl/movement/Scaffold.java
@@ -1,4 +1,16 @@
 package cn.exception.module.impl.movement;
 
-public class Scaffold {
+import cn.exception.event.EventTarget;
+import cn.exception.event.events.EventUpdate;
+import cn.exception.module.Module;
+
+public class Scaffold extends Module {
+    public Scaffold(){
+        super("Scaffold", Category.Movement);
+    }
+
+    @EventTarget
+    public void onEvent(EventUpdate e){
+
+    }
 }
